69bd4b18b012946d3be91dea4b19734df29dd848
Gail Hedani <gailhedani@Gails-MacBook-Pro-2.local>
2017-08-25 18:08:22 -1000

    Init chat app

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const express = require("express");
+
+const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+
+//console.log(__dirname + "/../public");
+// console.log(publicPath);
+
+// create static middleware to serve up the public folder
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+  console.log(`Server started at port ${port}`);
+});
