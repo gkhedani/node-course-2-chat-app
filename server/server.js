e49fc75ef8318e60d3cc5f4c474de4503e53c48a
Gail Hedani <gailhedani@Gails-MacBook-Pro-2.local>
2017-08-26 16:22:26 -1000

    Add connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 // chat-app for node course
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 
+// instead of using the express http server:
 var app = express();
+var server = http.createServer(app);
+// with this, we are ready to accept connections
+// we now have access to routes and a javascript library
+var io = socketIO(server);
 
 
 //console.log(__dirname + "/../public");
@@ -14,6 +21,16 @@ var app = express();
 // create static middleware to serve up the public folder
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// register an event listener
+io.on("connection", (socket) => {
+  console.log("New user connected");
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected from the client");
+  });
+});
+
+
+server.listen(port, () => {
   console.log(`Server started at port ${port}`);
 });
